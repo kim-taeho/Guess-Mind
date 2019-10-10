4833c86909d45e4d3c45a27db80ab443e9f87aee
kimtaeho <xxxxoghxxx@naver.com>
2019-10-11 04:09:39 +0900

    Gulp

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { join } from "path";
+import socketIO from "socket.io";
+import logger from "morgan";
+
+const app = express();
+const PORT = 4000;
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
+
+const handleListening = () => {
+    console.log(`Server running: http://localhost:${PORT}`);
+}
+
+app.get("/", (req, res) => res.render("home"));
+
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO.listen(server);
+
+io.on("connection", socket => {
+    socket.on("newMessage", ({ message }) => {
+        socket.broadcast.emit("messageNotif", { 
+            message, 
+            nickname: socket.nickname || "Anon"
+        });
+    });
+    socket.on("setNickname", ({ nickname }) => {
+        socket.nickname = nickname;
+    });
+});
